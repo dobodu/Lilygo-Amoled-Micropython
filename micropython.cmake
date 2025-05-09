8a743774d95df26c44424ae83d4350c6d204c303
dobodu <ludovic.boldu@sfr.fr>
2025-05-09 08:39:36 +0200

    Update micropython.cmake
    Correction to amoled directory

--- micropython.cmake
+++ micropython.cmake
@@ -3,4 +3,4 @@
 # Paths are absolute, and ${CMAKE_CURRENT_LIST_DIR} can be
 # used to prefix subdirectories.
 
-include(${CMAKE_CURRENT_LIST_DIR}/amoled_series/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/amoled/micropython.cmake)
